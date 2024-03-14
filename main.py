40ab3d49884a75856fe0059bcccbcb26432fbf6c
kululenko <alex.pmail@yahoo.com>
2024-03-14 16:14:39 +0100

    richtig easy sachen

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import csv
+import pandas
 
 
-#with open("weather_data.csv") as data:
-#    for lines in data.readlines():
-#        print(lines)
 
-with open("weather_data.csv") as data_file:
+
+with open("100days\\25days\\weather_data.csv") as data_file:
     data = csv.reader(data_file)
     temperatures = []
     for row in data:
         print(row)
         temperatures.append(row[1])
+    temperatures_int = [] 
+    for number in temperatures[1:]:
+        temperatures_int.append(int(number))
 
-    print(temperatures[1:])
+    print(temperatures_int)
+
+
+
---
+++ main2.py
@@ -0,0 +1,22 @@
+import pandas
+import pytemperature
+
+
+data = pandas.read_csv("100days\\25days\\weather_data.csv")
+#print(type(data))
+
+
+#data_dict = data.to_dict()
+#print(data_dict)
+
+#temp_list = data["temp"].to_list()
+
+#sum = 0
+#for x in temp_list:
+#    sum += x
+
+#print(sum // len(temp_list))
+
+#print(data["temp"].max())
+
+#print(data[data.temp == 24])
